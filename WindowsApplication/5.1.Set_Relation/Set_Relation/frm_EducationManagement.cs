d24eecf4137801b4b5726ed9805043f9da6a21fa
SmartLinli <linli@fjtcm.edu.cn>
2021-05-08 22:07:40 +0800

    udpate

--- WindowsApplication/5.1.Set_Relation/Set_Relation/frm_EducationManagement.cs
+++ WindowsApplication/5.1.Set_Relation/Set_Relation/frm_EducationManagement.cs
@@ -54,13 +54,11 @@ namespace Set_Relation
                 new DataRelation                                                                        //实例化数据关系，实现院系表、专业表之间的层次关系；
                     ("Department_Major",                                                                //数据关系名称；
                      departmentTable.Columns["No"],                                                     //父表的被参照列为院系表的编号列；
-                     majorTable.Columns["DepartmentNo"],                                                //子表的参照列为专业表的院系编号列；不要求后端数据库在子表的参照列上创建外键约束；
-                     false),                                                                            //不创建约束（父列上的唯一约束、子列上的外键约束）；
+                     majorTable.Columns["DepartmentNo"]),                                                //子表的参照列为专业表的院系编号列；不要求后端数据库在子表的参照列上创建外键约束；
                 new DataRelation                                                                        //实例化数据关系，实现专业表、班级表之间的层次关系；
                     ("Major_Class",                                                                     //数据关系名称；
                      majorTable.Columns["No"],                                                          //父表的被参照列为专业表的编号列；
-                     classTable.Columns["MajorNo"],                                                     //子表的参照列为班级表的专业编号列；
-                     false)                                                                             //不创建约束（父列上的唯一约束、子列上的外键约束）；
+                     classTable.Columns["MajorNo"])                                                     //子表的参照列为班级表的专业编号列；
             };
             dataSet.Relations.AddRange(dataRelations);                                                  //将数据关系数组批量加入数据集的关系集合中；
             this.trv_EducationUnit.Nodes.Clear();                                                       //树形视图的节点集合清空；
