bccb62e4de46c6a1a25c40d2efd6e1b61d81cba3
SmartLinli <linli@fjtcm.edu.cn>
2021-05-12 00:22:58 +0800

    update

--- WindowsApplication/6.2.ObjectOriented_Layer/ObjectOriented_Layer/Model/User.cs
+++ WindowsApplication/6.2.ObjectOriented_Layer/ObjectOriented_Layer/Model/User.cs
@@ -1,41 +1,25 @@
 ﻿namespace ObjectOriented_Layer
-{    
+{
     /// <summary>
     /// 用户；
     /// </summary>
     public class User
-    {        
+    {
         /// <summary>
         /// 用户号；
         /// </summary>
-        public string No
-        {
-            get;
-            set;
-        }
+        public string No { get; set; }
         /// <summary>
         /// 密码（加密）；
         /// </summary>
-        public byte[] Password
-        {
-            get;
-            set;
-        }
-		/// <summary>
-		/// 是否激活；
-		/// </summary>
-		public bool IsActivated
-		{
-			get;
-			set;
-		}
-		/// <summary>
-		/// 登录错误次数；
-		/// </summary>
-		public int LoginFailCount
-		{
-			get;
-			set;
-		}
-	}
+        public byte[] Password { get; set; }
+        /// <summary>
+        /// 是否激活；
+        /// </summary>
+        public bool IsActivated { get; set; }
+        /// <summary>
+        /// 登录错误次数；
+        /// </summary>
+        public int LoginFailCount { get; set; }
+    }
 }
