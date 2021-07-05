a60ab838ce5b10182041e32d0bbdf0c4a68eb1d9
SmartLinli <linli@fjtcm.edu.cn>
2021-07-05 23:43:18 +0800

    update

--- WindowsApplication/7.2.EntityFramework_Projection/EntityFramework_Projection/Ui/frm_EducationManagement.designer.cs
+++ WindowsApplication/7.2.EntityFramework_Projection/EntityFramework_Projection/Ui/frm_EducationManagement.designer.cs
@@ -28,76 +28,76 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.trv_EducationUnit = new System.Windows.Forms.TreeView();
-			this.dgv_Student = new System.Windows.Forms.DataGridView();
-			this.btn_Edit = new System.Windows.Forms.Button();
-			this.btn_Add = new System.Windows.Forms.Button();
-			this.btn_Delete = new System.Windows.Forms.Button();
-			this.BackgroundWorker = new System.ComponentModel.BackgroundWorker();
-			((System.ComponentModel.ISupportInitialize)(this.dgv_Student)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// trv_EducationUnit
-			// 
-			this.trv_EducationUnit.Location = new System.Drawing.Point(9, 12);
-			this.trv_EducationUnit.Name = "trv_EducationUnit";
-			this.trv_EducationUnit.Size = new System.Drawing.Size(139, 174);
-			this.trv_EducationUnit.TabIndex = 0;
-			this.trv_EducationUnit.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.trv_EducationUnit_AfterSelect);
-			// 
-			// dgv_Student
-			// 
-			this.dgv_Student.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dgv_Student.Location = new System.Drawing.Point(167, 12);
-			this.dgv_Student.Name = "dgv_Student";
-			this.dgv_Student.RowTemplate.Height = 23;
-			this.dgv_Student.Size = new System.Drawing.Size(145, 174);
-			this.dgv_Student.TabIndex = 1;
-			// 
-			// btn_Edit
-			// 
-			this.btn_Edit.Location = new System.Drawing.Point(237, 192);
-			this.btn_Edit.Name = "btn_Edit";
-			this.btn_Edit.Size = new System.Drawing.Size(75, 23);
-			this.btn_Edit.TabIndex = 2;
-			this.btn_Edit.Text = "编辑";
-			this.btn_Edit.UseVisualStyleBackColor = true;
-			this.btn_Edit.Click += new System.EventHandler(this.btn_Edit_Click);
-			// 
-			// btn_Add
-			// 
-			this.btn_Add.Location = new System.Drawing.Point(156, 192);
-			this.btn_Add.Name = "btn_Add";
-			this.btn_Add.Size = new System.Drawing.Size(75, 23);
-			this.btn_Add.TabIndex = 4;
-			this.btn_Add.Text = "添加";
-			this.btn_Add.UseVisualStyleBackColor = true;
-			this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
-			// 
-			// btn_Delete
-			// 
-			this.btn_Delete.Location = new System.Drawing.Point(156, 221);
-			this.btn_Delete.Name = "btn_Delete";
-			this.btn_Delete.Size = new System.Drawing.Size(75, 23);
-			this.btn_Delete.TabIndex = 5;
-			this.btn_Delete.Text = "删除";
-			this.btn_Delete.UseVisualStyleBackColor = true;
-			this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
-			// 
-			// frm_EducationManagement
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(324, 246);
-			this.Controls.Add(this.btn_Delete);
-			this.Controls.Add(this.btn_Add);
-			this.Controls.Add(this.btn_Edit);
-			this.Controls.Add(this.dgv_Student);
-			this.Controls.Add(this.trv_EducationUnit);
-			this.Name = "frm_EducationManagement";
-			this.Text = "教学管理";
-			((System.ComponentModel.ISupportInitialize)(this.dgv_Student)).EndInit();
-			this.ResumeLayout(false);
+            this.trv_EducationUnit = new System.Windows.Forms.TreeView();
+            this.dgv_Student = new System.Windows.Forms.DataGridView();
+            this.btn_Edit = new System.Windows.Forms.Button();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.BackgroundWorker = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Student)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trv_EducationUnit
+            // 
+            this.trv_EducationUnit.Location = new System.Drawing.Point(9, 12);
+            this.trv_EducationUnit.Name = "trv_EducationUnit";
+            this.trv_EducationUnit.Size = new System.Drawing.Size(139, 174);
+            this.trv_EducationUnit.TabIndex = 0;
+            this.trv_EducationUnit.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.trv_EducationUnit_AfterSelect);
+            // 
+            // dgv_Student
+            // 
+            this.dgv_Student.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Student.Location = new System.Drawing.Point(167, 12);
+            this.dgv_Student.Name = "dgv_Student";
+            this.dgv_Student.RowTemplate.Height = 23;
+            this.dgv_Student.Size = new System.Drawing.Size(161, 174);
+            this.dgv_Student.TabIndex = 1;
+            // 
+            // btn_Edit
+            // 
+            this.btn_Edit.Location = new System.Drawing.Point(253, 192);
+            this.btn_Edit.Name = "btn_Edit";
+            this.btn_Edit.Size = new System.Drawing.Size(75, 23);
+            this.btn_Edit.TabIndex = 2;
+            this.btn_Edit.Text = "编辑";
+            this.btn_Edit.UseVisualStyleBackColor = true;
+            this.btn_Edit.Click += new System.EventHandler(this.btn_Edit_Click);
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Location = new System.Drawing.Point(167, 192);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(75, 23);
+            this.btn_Add.TabIndex = 4;
+            this.btn_Add.Text = "添加";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Location = new System.Drawing.Point(167, 221);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(75, 23);
+            this.btn_Delete.TabIndex = 5;
+            this.btn_Delete.Text = "删除";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // frm_EducationManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 246);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.btn_Edit);
+            this.Controls.Add(this.dgv_Student);
+            this.Controls.Add(this.trv_EducationUnit);
+            this.Name = "frm_EducationManagement";
+            this.Text = "教学管理";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Student)).EndInit();
+            this.ResumeLayout(false);
 
 		}
 
