6acb67fefb458dd3922f14e634761d637be338a0
SmartLinli <linli@fjtcm.edu.cn>
2022-04-12 15:45:34 +0800

    update

--- WindowsApplication/4.4.Table_Row/Table_Row/frm_CourseSelection.cs
+++ WindowsApplication/4.4.Table_Row/Table_Row/frm_CourseSelection.cs
@@ -123,7 +123,7 @@ namespace Table_Row
                 return;                                                                                     //返回；
             string courseNo = selectedCourseRow["No"].ToString();											//获取当前课程数据行的课程编号；
             DataRow deletedCourseRow =																		//声明已删课程数据行（即先前从课程数据表中删除的数据行）；
-                this.CourseTable.Select($"No='{courseNo}'", "", DataViewRowState.Deleted)[0];			//已删课程数据行可通过课程数据表的方法Select查得，该方法接受查询条件、排序条件、行状态条件等参数，并返回数据行数组；
+                this.CourseTable.Select($"No='{courseNo}'", "", DataViewRowState.Deleted)[0];			    //已删课程数据行可通过课程数据表的方法Select查得，该方法接受查询条件、排序条件、行状态条件等参数，并返回数据行数组；
             deletedCourseRow.RejectChanges();																//已删课程数据行拒绝更改，即回滚先前对其执行的删除；随后该行的状态为未更改；
             this.SelectedCourseTable.Rows.Remove(selectedCourseRow);										//从已选课程数据表的行集合中移除当前课程数据行；随后该行的状态为分离；
             this.lbl_CreditSum.Text =																		//在标签中显示已选课程的学分总和；
